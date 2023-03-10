9ba35da6797d54318a589bb64f534fb132b0109e
Hirou <82706798+thehirou@users.noreply.github.com>
2023-03-10 16:05:23 +0100

    messing around with javascript

---
+++ script.js
@@ -0,0 +1,20 @@
+var number=10;
+var string="helloooooo :3";
+var isRad=true;
+
+
+
+
+function listGroceries()
+{
+
+    for(var i=0; i<10; i++)
+    {
+    console.log(i)
+    }
+
+}
+
+listGroceries();
+
+document.getElementById("randomBox1").addEventListener("click", function(){alert("click hehehe");})
